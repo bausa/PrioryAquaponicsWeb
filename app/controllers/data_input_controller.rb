ee8d07de94a10d9ec5e1dc582ce88e59968092b7
Sam Baumgarten <sambaumgarten98@gmail.com>
2015-06-01 20:28:33 -0700

    init

---
+++ app/controllers/data_input_controller.rb
@@ -0,0 +1,11 @@
+class DataInputController < ApplicationController
+  def logData
+    dp = Datapoint.new;
+    dp.date = DateTime.now
+    dp.sensor = params[:sensor]
+    dp.value = params[:value]
+    dp.save
+    
+    render :text => "Ok"
+  end
+end
